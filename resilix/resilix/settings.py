dff05e8ea67ca03295e7e74dbe1e493b3759171f
EdiEdison <ediedisonfornang@gmail..com>
2023-11-30 10:19:46 +0100

    updates

--- resilix/resilix/settings.py
+++ resilix/resilix/settings.py
@@ -130,11 +130,13 @@ USE_I18N = True
 
 USE_TZ = True
 
+import os
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfile")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
